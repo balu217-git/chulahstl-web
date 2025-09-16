8c2238358120e25579c43e3606477b59b50b8ed6
balu217-git <vangapandubalaraju.git@gmail.com>
2025-09-16 17:21:10 +0530

    ccc

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import Slider from "react-slick";
+import Slider, { Settings } from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
@@ -14,7 +14,7 @@ type Slide = {
 
 type HeroSliderProps = {
   slides: Slide[];
-  settings?: Record<string, any>;
+  settings?: Settings;
 };
 
 
