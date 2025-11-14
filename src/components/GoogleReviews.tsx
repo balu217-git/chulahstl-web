3ca5e8402175726d66e4b423e2a9efaafd6b4d72
balu217-git <vangapandubalaraju.git@gmail.com>
2025-11-14 18:35:52 +0530

    dsdsd

--- src/components/GoogleReviews.tsx
+++ src/components/GoogleReviews.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.css";
 
 interface Review {
   author_name: string;
